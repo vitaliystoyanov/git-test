14ed7f1f10cfbbd00c38dd99312cd985afad0a96
Vitaliy <developer.stoyanov@gmail.com>
2017-01-31 20:52:03 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        //test merge
-        //test master
-        //test test branch
     }
 }
