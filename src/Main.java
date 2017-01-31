eda79545682ed5e74325bbecc6649388bdd10327
Vitaliy <developer.stoyanov@gmail.com>
2017-01-31 21:23:21 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        //new test branch 123
+        //new test branch 123 456
     }
 }
